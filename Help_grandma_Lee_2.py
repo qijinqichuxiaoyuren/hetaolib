a23baa11b5e84be457d5486572f839f31c559456
xdyan1991 <xdyan_1991@163.com>
2018-09-23 02:23:54 +0800

    remove answer

--- Help_grandma_Lee_2.py
+++ Help_grandma_Lee_2.py
@@ -4,10 +4,7 @@ from hetaolib import *
 #李奶奶每次会把数据data传递给你， 如果data是一个小数，试着把它转换为整数return给李奶奶，不是小数的话原样返回就好了
 def convert_float_to_int(data):
     #↓type your code below
-    if type(data) == float:
-        return int(data)
-    else:
-        return data
+
 
 
 
